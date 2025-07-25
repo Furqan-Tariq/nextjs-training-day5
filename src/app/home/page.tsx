e7d1404ee06899986a9da0c4ae5a9de431d96a76
Furqan-Tariq <furqant.ft@gmail.com>
2025-07-25 15:11:24 +0500

    navbar fixed in layout.tsx rather than in each page

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,9 +1,9 @@
-import Navigation from "@/app/components/navigation"
+//import Navigation from "@/app/components/navigation"
 
 export default function HomePage() {
   return (
     <div className="min-h-screen bg-gray-50">
-      <Navigation />
+      {/* <Navigation /> */}
 
       <main className="container mx-auto px-4 py-8">
         <div className="max-w-4xl mx-auto">
